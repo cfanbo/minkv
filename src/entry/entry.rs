c3c6f00938d38e6cc347ee2331f899aa581f70ab
cfanbo <haohtml@gmail.com>
2024-08-23 16:23:25 +0800

    remove unused unittest

--- src/entry/entry.rs
+++ src/entry/entry.rs
@@ -51,11 +51,6 @@ impl Op {
         Op::from_u8(value).ok_or("Invalid value for Op")
     }
 }
-// impl Default for Op {
-//     fn default() -> Self {
-//         Op::Add
-//     }
-// }
 
 // crc (4 bytes) | timestamp (8 bytes) | key_size (4 bytes) | value_size (8 bytes) | op (1 bytes) |  key | value
 #[derive(Default)]
@@ -246,98 +241,6 @@ impl From<EntryFile> for Vec<EntryParseResult> {
         results
     }
 }
-
-// impl Into<Vec<EntryParseResult>> for EntryFile {
-//     fn into(mut self) -> Vec<EntryParseResult> {
-//         // crc (4 bytes) | timestamp (8 bytes) | key_size (4 bytes) | value_size (8 bytes) | op (1 bytes) | key | value
-//         let header_size = Entry::default().header_size();
-//         let mut buffer = vec![0; header_size];
-
-//         let _ = self.0.seek(SeekFrom::Start(0));
-
-//         let mut offset: usize = 0;
-//         let mut results = Vec::new();
-//         loop {
-//             //  crc = 4 |  time = 8 |  ksize = 4 | vsize = 8 | op = 1
-//             match self.0.read_exact(&mut buffer) {
-//                 Ok(()) => {
-//                     // 读取成功，处理 buffer 的内容
-//                     //debug!("Successfully read {} bytes", buffer.len());
-//                     // 处理 buffer 内容的其他操作
-//                 }
-//                 Err(ref e) if e.kind() == io::ErrorKind::UnexpectedEof => {
-//                     // 文件结束EOF，处理不完整数据
-//                     // debug!("Reached end of file");
-//                     break; // 退出循环
-//                 }
-//                 Err(e) => {
-//                     // 处理其他 I/O 错误
-//                     debug!("Error reading file: {}", e);
-//                     break; // 退出循环
-//                 }
-//             }
-
-//             let crc = u32::from_le_bytes(
-//                 buffer[..4]
-//                     .try_into()
-//                     .map_err(|_| "Invalid CRC data")
-//                     .unwrap(),
-//             );
-//             let timestamp = u64::from_le_bytes(
-//                 buffer[4..12]
-//                     .try_into()
-//                     .map_err(|_| "Invalid timestamp data")
-//                     .unwrap(),
-//             );
-//             let key_size = u32::from_le_bytes(
-//                 buffer[12..16]
-//                     .try_into()
-//                     .map_err(|_| "Invalid key_size data")
-//                     .unwrap(),
-//             );
-//             let value_size = u64::from_le_bytes(
-//                 buffer[16..24]
-//                     .try_into()
-//                     .map_err(|_| "Invalid value_size data")
-//                     .unwrap(),
-//             );
-
-//             let op = Op::from_le_bytes(
-//                 buffer[24..25]
-//                     .try_into()
-//                     .map_err(|_| "Invalid value_size data")
-//                     .unwrap(),
-//             )
-//             .unwrap();
-
-//             // key
-//             let mut key = vec![0; key_size as usize];
-//             self.0.read_exact(&mut key).unwrap();
-
-//             // value
-//             let mut value = vec![0; value_size as usize];
-//             self.0.read_exact(&mut value).unwrap();
-
-//             let entry = Entry {
-//                 crc,
-//                 timestamp,
-//                 key_size,
-//                 value_size,
-//                 op,
-//                 key,
-//                 value,
-//             };
-
-//             results.push(EntryParseResult {
-//                 value_pos: offset as u64,
-//                 entry,
-//             });
-//             offset += header_size + key_size as usize + value_size as usize;
-//         }
-
-//         results
-//     }
-// }
 
 impl fmt::Debug for Entry {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
@@ -538,27 +441,6 @@ impl From<Entry> for Vec<u8> {
         result
     }
 }
-
-// Into trait
-// impl Into<Vec<u8>> for Entry {
-//     fn into(self) -> Vec<u8> {
-//         // 计算总容量（4 字段的长度 + key 和 value 的长度）
-//         let total_size = Entry::default().size();
-//         println!("inro {:?}", self.op.to_le_bytes());
-//         println!("inro2 {:?}", self.key_size.to_le_bytes());
-
-//         let mut result = Vec::with_capacity(total_size);
-//         result.extend_from_slice(&self.crc.to_le_bytes());
-//         result.extend_from_slice(&self.timestamp.to_le_bytes());
-//         result.extend_from_slice(&self.key_size.to_le_bytes());
-//         result.extend_from_slice(&self.value_size.to_le_bytes());
-//         result.extend_from_slice(&self.op.to_le_bytes());
-//         result.extend(self.key);
-//         result.extend(self.value);
-//         println!("{:?}", result);
-//         result
-//     }
-// }
 
 #[cfg(test)]
 mod tests {
--- src/entry/hint.rs
+++ src/entry/hint.rs
@@ -26,20 +26,6 @@ impl From<Hint> for Vec<u8> {
         result
     }
 }
-// impl Into<Vec<u8>> for Hint {
-//     fn into(self) -> Vec<u8> {
-//         let mut result: Vec<u8> = Vec::with_capacity(28 + self.key_size as usize);
-
-//         // 将各个字段的字节添加到结果中
-//         result.extend_from_slice(&self.timestamp.to_le_bytes());
-//         result.extend_from_slice(&self.key_size.to_le_bytes());
-//         result.extend_from_slice(&self.value_size.to_le_bytes());
-//         result.extend_from_slice(&self.value_pos.to_le_bytes());
-//         result.extend_from_slice(&self.key);
-
-//         result
-//     }
-// }
 
 // | timestamp (8 bytes) | key_size (4 bytes) | value_size (8 bytes) | value_pos (8 bytes)  | key |
 pub struct HintFile(File);
@@ -190,74 +176,6 @@ impl From<HintFile> for Vec<Hint> {
         hits_result
     }
 }
-
-// impl Into<Vec<Hint>> for HintFile {
-//     fn into(mut self) -> Vec<Hint> {
-//         let mut buffer = vec![0; 28];
-//         let mut hits_result = Vec::new();
-//         loop {
-//             //  crc = 4 |  time = 8 |  ksize = 4 | vsize = 8
-//             match self.0.read_exact(&mut buffer) {
-//                 Ok(()) => {
-//                     // 读取成功，处理 buffer 的内容
-//                     // println!("Successfully read {} bytes", buffer.len());
-//                     // 处理 buffer 内容的其他操作
-//                 }
-//                 Err(ref e) if e.kind() == io::ErrorKind::UnexpectedEof => {
-//                     // 文件结束EOF，处理不完整数据
-//                     println!("Reached end of file");
-//                     break; // 退出循环
-//                 }
-//                 Err(e) => {
-//                     // 处理其他 I/O 错误
-//                     eprintln!("Error reading file: {}", e);
-//                     break; // 退出循环
-//                 }
-//             }
-
-//             let timestamp = u64::from_le_bytes(
-//                 buffer[0..8]
-//                     .try_into()
-//                     .map_err(|_| "Invalid timestamp data")
-//                     .unwrap(),
-//             );
-//             let key_size = u32::from_le_bytes(
-//                 buffer[8..12]
-//                     .try_into()
-//                     .map_err(|_| "Invalid key_size data")
-//                     .unwrap(),
-//             );
-//             let value_size = u64::from_le_bytes(
-//                 buffer[12..20]
-//                     .try_into()
-//                     .map_err(|_| "Invalid value_size data")
-//                     .unwrap(),
-//             );
-
-//             // value_pos
-//             let value_pos = u64::from_le_bytes(
-//                 buffer[20..28]
-//                     .try_into()
-//                     .map_err(|_| "Invalid value_size data")
-//                     .unwrap(),
-//             );
-
-//             // key 再次读取指定长度的字节
-//             let mut key = vec![0; key_size as usize];
-//             self.0.read_exact(&mut key).unwrap();
-
-//             hits_result.push(Hint {
-//                 timestamp,
-//                 key_size,
-//                 value_size,
-//                 value_pos,
-//                 key,
-//             })
-//         }
-
-//         hits_result
-//     }
-// }
 
 #[cfg(test)]
 pub mod tests {
