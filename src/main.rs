af2fc73230ec44023e8a07cabdc6d74881fb6632
cfanbo <haohtml@gmail.com>
2024-08-26 17:14:49 +0800

    chore: remove unused code

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,13 @@
-use log::debug;
+use log::error;
 use minkv::cli;
-use serde::{Deserialize, Serialize};
-
-// 派生 Serialize 和 Deserialize
-#[derive(Serialize, Deserialize)]
-struct Record {
-    id: u32,
-    name: String,
-    age: u8,
-}
 
 #[tokio::main]
 async fn main() {
     match cli::cli::parse().await {
         Ok(_) => {}
         Err(e) => {
-            debug!("{:?}", e);
-            println!("{:?}", e)
+            error!("{:?}", e);
+            eprintln!("{:?}", e)
         }
     }
 }
